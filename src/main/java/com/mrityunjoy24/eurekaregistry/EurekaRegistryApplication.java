d8c7fcfbaef56a1896b4d851b2be945cd7d90e58
Mrityunjoy Kumar <mrityunjoykumar824@gmail.com>
2023-11-11 15:18:43 +0530

    first commit

---
+++ src/main/java/com/mrityunjoy24/eurekaregistry/EurekaRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.mrityunjoy24.eurekaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrityunjoy24/eurekaregistry/EurekaRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrityunjoy24.eurekaregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
